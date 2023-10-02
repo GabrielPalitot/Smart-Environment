1b8ae5f6ac295baefb0f34b3ec927f7c8224abfb
LucasdeAl <lucasde@alu.ufc.br>
2023-10-02 20:25:11 -0300

    SensorUDP working!

--- src/main/java/gateway/SensorUDP.java
+++ src/main/java/gateway/SensorUDP.java
@@ -2,28 +2,50 @@ package gateway;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-public class SensorUDP {
-    private DatagramSocket socket;
-    private InetAddress address;
+public class SensorUDP extends Thread {
+    private static DatagramSocket socket;
+    private static InetAddress address;
 
-    private byte[] buf;
+    private static byte[] buf ;
 
     public SensorUDP() throws SocketException, UnknownHostException {
         socket = new DatagramSocket();
         address = InetAddress.getByName("localhost");
     }
 
-    public String sendEcho(String msg) throws IOException {
-        buf = msg.getBytes();
-        DatagramPacket packet
-                = new DatagramPacket(buf, buf.length, address, 4445);
-        socket.send(packet);
-        packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
-        String received = new String(
-                packet.getData(), 0, packet.getLength());
-        return received;
+    public static void main(String argv[]) throws SocketException, UnknownHostException {
+
+        SensorUDP sensor = new SensorUDP();
+        while(true)
+        {
+
+            String msg = "27.0\0";
+            buf = msg.getBytes();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            //ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+            //scheduler.schedule(() -> {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length, sensor.address, 4445);
+               try {
+                    sensor.socket.send(packet);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+              //  System.out.println("Task executed!");
+            //}, 2, TimeUnit.SECONDS); // Delay of 2 seconds
+
+            //packet = new DatagramPacket(buf, buf.length);
+            //socket.receive(packet);
+            //String received = new String(packet.getData(), 0, packet.getLength());
+        }
     }
 
     public void close() {
--- src/main/java/gateway/SimpleProtobufTCP_UDPServer.java
+++ src/main/java/gateway/SimpleProtobufTCP_UDPServer.java
@@ -1,24 +1,43 @@
 package gateway;
 import java.io.*;
 import java.net.*;
-
-
 import com.google.protobuf.CodedInputStream;
-
 import gateway.PessoaPB.Pessoa;
 
-public class SimpleProtobufTCP_UDPServer {
-	public static void main(String argv[]) {
-		ServerSocket listenSocket;
+
+public class SimpleProtobufTCP_UDPServer extends Thread {
+
+	private DatagramSocket socket;
+	private ServerSocket listenSocket;
+	private static byte[] buf = new byte[256];
+
+
+	public SimpleProtobufTCP_UDPServer() throws IOException {
+		socket = new DatagramSocket(4445);
+		listenSocket = new ServerSocket(6789);
+	}
+		public static void main(String argv[]) {
+
 		try {
-			listenSocket = new ServerSocket(6789);
+			SimpleProtobufTCP_UDPServer server = new SimpleProtobufTCP_UDPServer();
+
 			while (true) {
-				Socket connectionSocket = listenSocket.accept();
-				CodedInputStream in = CodedInputStream.newInstance(connectionSocket.getInputStream());
-				int size = in.readInt32();
-				Pessoa p = Pessoa.parseFrom(in);
-				System.out.println(p.toString());
-				System.out.println(size);
+
+
+
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				server.socket.receive(packet);
+				InetAddress address = packet.getAddress();
+				int port = packet.getPort();
+				packet = new DatagramPacket(buf, buf.length, address, port);
+				String received = new String(packet.getData(), 0, packet.getLength());
+				System.out.println(received);
+				//Socket connectionSocket = listenSocket.accept();
+				//CodedInputStream in = CodedInputStream.newInstance(connectionSocket.getInputStream());
+				//int size = in.readInt32();
+				//Pessoa p = Pessoa.parseFrom(in);
+				//System.out.println(p.toString());
+				//System.out.println(size);
 
 				//ThreadSockets thread = new ThreadSockets(connectionSocket);
 
