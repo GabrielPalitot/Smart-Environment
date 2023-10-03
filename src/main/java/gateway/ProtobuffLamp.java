52d8b828e17c28333bb409b50ee59dbd602cdacf
LucasdeAl <lucasde@alu.ufc.br>
2023-10-03 20:55:22 -0300

    ProtobuffLamp added

---
+++ src/main/java/gateway/ProtobuffLamp.java
@@ -0,0 +1,53 @@
+package gateway;
+
+import com.house.objects.Lamp;
+import java.io.IOException;
+import java.net.*;
+
+
+public class ProtobuffLamp {
+    public enum StatusLamp {
+        TURNED_ON,
+        TURNED_OFF,
+        MALFUNCTION
+    }
+    private boolean connected;
+    private StatusLamp status;
+    private int temperature;
+
+    public ProtobuffLamp(boolean connected, StatusLamp status) {
+        this.connected = connected;
+        this.status = status;
+    }
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public void setConnected(boolean connected) {
+        this.connected = connected;
+    }
+
+    public StatusLamp getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusLamp status) {
+        this.status = status;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+
+    public static void main(String[] args) {
+        ProtobuffLamp air = new ProtobuffLamp(true, ProtobuffLamp.StatusLamp.TURNED_OFF);
+
+    }
+}
+
+
